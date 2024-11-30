8181437944f51367f39e1688219f4af8b44e8250
ChaoYiChen <a951159aca@gmail.com>
2024-11-30 12:44:07 -0800

    Modify MCP3008 object.

--- potentiometer_mcp3008.py
+++ potentiometer_mcp3008.py
@@ -1,14 +1,22 @@
 from gpiozero import (
     MCP3008,
 )  # Import the MCP3008 class from gpiozero for handling analog input.
+from time import sleep  # Import sleep to add a delay in the program.
 
 
 def main():
-    pot = MCP3008(channel=0)  # Create an MCP3008 object for the specified channel.
+    channel = int(input("Channel: "))  # Ask the user for the MCP3008 channel number.
+
+    pot = MCP3008(
+        channel=channel,
+        max_voltage=5.0,
+    )  # Create an MCP3008 object for the specified channel.
 
     while True:
-        voltage = pot.value
-        print(f"Voltage: {voltage:.2f} V")
+        value = pot.value
+        voltage = pot.voltage
+        print(f"Value: {value:.2f}, Voltage: {voltage:.2f} V")
+        sleep(0.5)
 
 
 if __name__ == "__main__":
